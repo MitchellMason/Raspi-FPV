954606a3724a398a3f0d9c4dbf53fc9a4fc9369d
Mitchell Mason <thefreemason11@gmail.com>
2017-06-20 14:39:13 -0400

    Recv now displays video underneath tel data. Tel data doesn't yet render correctly, all still single thread

--- Rx/FPV_PFD/data/prime.bash
+++ Rx/FPV_PFD/data/prime.bash
@@ -1,8 +1,14 @@
-#primes the antenna to receive/transmit data over monitor
+#primes the antenna to receive/transmit data over monitor, and 
+#establishes fifo pipes
+
+sudo rm /tmp/tel
+sudo rm /tmp/vid
+sudo mkfifo -m 777 /tmp/tel
+sudo mkfifo -m 777 /tmp/vid
 
 sudo killall ifplugd #stop management of interface
 sudo ifconfig wlan_fpv down
 sudo iw dev wlan_fpv set monitor otherbss fcsfail
 sudo ifconfig wlan_fpv up
 sudo iwconfig wlan_fpv channel 13
-echo "	prime.bash: Antenna ready"
+echo "	prime.bash: Antennas ready, fifos up"
---
+++ Rx/test_video.bash
@@ -0,0 +1,11 @@
+#Used to ensure proper data flow from the drone camera and test latency. 
+
+#prime the antenna
+sudo killall ifplugd #stop management of interface
+sudo ifconfig wlan_fpv down
+sudo iw dev wlan_fpv set monitor otherbss fcsfail
+sudo ifconfig wlan_fpv up
+sudo iwconfig wlan_fpv channel 13
+
+#Start the video
+sudo rx -b 8 -r 4 -f 1450 -p 0 wlan_fpv | /opt/vc/src/hello_pi/hello_video/hello_video.bin
